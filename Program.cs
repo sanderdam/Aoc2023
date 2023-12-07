38e2e0e8a808ec534c1119e510ee139168b11299
Sander Dam <7230992+sanderdam@users.noreply.github.com>
2023-12-07 21:38:15 +0100

    Commited day 1

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOfCode;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //new Day1().FirstPart();
+        new Day1().SecondPart();
+    }
+}
---
+++ Solvers/Day1.cs
@@ -0,0 +1,69 @@
+public class Day1
+{
+    public void FirstPart()
+    {
+        string[] input = GetPuzzleInput();
+
+        int total = 0;
+        foreach (var line in input)
+        {
+            char firstDigit = line.First(x => char.IsDigit(x));
+            char secondDigit = line.Reverse().First(x => char.IsDigit(x));
+            char[] digitCombined = { firstDigit, secondDigit };
+            string twoDigitNumber = new string(digitCombined);
+
+            int number = int.Parse(twoDigitNumber);
+            total += number;
+        }
+
+        Console.WriteLine("Total is " + total);
+    }
+
+    public void SecondPart()
+    {
+        string[] input = GetPuzzleInput();
+
+        int total = 0;
+        foreach (string line in input)
+        {
+            string lineReplaced = ReplaceNumberTextInLine(line);
+            char firstDigit = lineReplaced.First(x => char.IsDigit(x));
+            char secondDigit = lineReplaced.Reverse().First(x => char.IsDigit(x));
+            char[] digitCombined = { firstDigit, secondDigit };
+            string twoDigitNumber = new string(digitCombined);
+
+            int number = int.Parse(twoDigitNumber);
+            total += number;
+        }
+
+        Console.WriteLine("Total is " + total);
+    }
+
+    private IDictionary<string,string> TextToNumbersMap = new Dictionary<string,string>(){
+            { "one","o1e"},
+            { "two","t2o"},
+            { "three","t3ree"},
+            { "four","f4ur"},
+            { "five","f5ve"},
+            { "six","s6x"},
+            { "seven","s7ven"},
+            { "eight","e8ght"},
+            { "nine","n9ne"},            
+            { "zero","z0ro"}
+    };
+
+    private string ReplaceNumberTextInLine(string line) {
+        
+        foreach( var map in TextToNumbersMap){
+            line = line.Replace(map.Key, map.Value);
+        }
+        
+        return line;
+    }
+
+    private string[] GetPuzzleInput()
+    {
+        string[] input = File.ReadAllLines(".\\..\\..\\..\\Inputs\\day1.txt");
+        return input;
+    }
+}
